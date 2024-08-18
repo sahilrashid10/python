2ac65f378453e4e83e34004bb62b02d90ec67c29
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2024-08-18 12:06:16 +0530

    Update and rename CYPHER_TEXTING.PY to CYPHER_TEXTING.py

---
+++ CYPHER_TEXTING.py
@@ -0,0 +1,27 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    extras = ""
+    for letter in original_text:
+        if letter not in alphabet:
+            extras += letter
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position %= len(alphabet)
+        output_text += alphabet[shifted_position]
+    output_text  += extras
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
+print("************Welcome to SR7 cypher texting **************")
+flag=""
+while flag !="exit":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    flag = input("Press exit to stop or any key to continue.").lower
+
+
+
