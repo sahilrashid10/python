c30e8bde70a7a165e3c4ef2879b05df9c580eadc
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2024-08-18 12:06:54 +0530

    Add files via upload

---
+++ BID_GAME.py
@@ -0,0 +1,53 @@
+import random
+things = [
+    "Vintage Vinyl Records",
+    "Antique Furniture",
+    "Rare Comic Books",
+    "Gold Jewelry",
+    "Collectible Coins",
+    "Fine Art Paintings",
+    "Designer Handbags",
+    "Classic Cars",
+    "Sports Memorabilia",
+    "Vintage Watches",
+    "Luxury Watches",
+    "Historical Artifacts",
+    "Mid-Century Modern Furniture",
+    "Rare Stamps",
+    "High-End Electronics",
+    "Limited Edition Prints",
+    "Old Maps",
+    "Sculptures",
+    "Persian Rugs",
+    "Unique Pottery",
+    "Musical Instruments"
+]
+
+thing = random.choice(things)
+auction_dictionary={}
+print("Welcome to SR7 auction")
+flag = True
+while flag:
+    name = input("Enter your name: ")
+    price = int(input(f"Enter price for the auction of {thing} $: "))
+    auction_dictionary[name]=price
+    print("More bidders?")
+    x=""
+    while x != "stop":
+        x = input("yes to continue, no to see the result: ")
+        if x == "yes":
+            print("\n"*10)
+            x="stop"
+        elif x == "no":
+            flag = False
+            print("\n"*10)
+            max_bid = -1
+            winner = ""
+            for key in auction_dictionary:
+                if auction_dictionary[key] > max_bid:
+                    max_bid = auction_dictionary[key]
+                    winner = key
+            print(f"The winner is {winner} with a bid of ${max_bid}:")
+            x="stop"
+        else:
+            print("********INVALID INPUT********* \n\n\n")
