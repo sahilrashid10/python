173617680e1d31e0f82d567e5740920bf3afe1ab
SAHIL RASHID <154680964+sahilrashid10@users.noreply.github.com>
2024-08-20 12:10:40 +0530

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,32 +1,40 @@
 import random
+
 def compare(x,y):
     if x>y:
-        print("too high\n")
+        print("too high\n\n")
     elif x<y:
-        print("too low\n")
+        print("too low\n\n")
     else :
-        print("you win😍\n")
+        print("you win😍\n\n")
         return True
     return False
 
-number= random.randint(1,100)
-level = input("Lets play number guessing game, type 'easy'👍 or 'hard'👾: ")
-attempts = 0
-emojis = []
-if level == 'easy':
-    attempts = 10
-    emojis = ['😭', '😢', '☹️', '🙁', '😕', '😐', '🙂', '😊', '😃', '😁']
+def set_difficulty(l,a,e):
+    if l == 'easy':
+        a = 10
+        e = ['😭', '😢', '☹️', '🙁', '😕', '😐', '🙂', '😊', '😃', '😁']
 
-elif level == 'hard':
-    attempts = 5
-    emojis =  ['😭', '😢', '🙁', '🙂', '😁']
-for i in range(attempts,0,-1):
-    print(f"you have {i},{emojis[i-1]} attempts left!")
-    guess = int(input("guess the number :"))
-    if compare(guess, number):
-        break
-    elif i==1:
-        print("You lose🧟‍♂️")
+    elif l == 'hard':
+        a = 5
+        e =  ['😭', '😢', '🙁', '🙂', '😁']
+    return l,a,e
 
+def game():
+    number= random.randint(1,100)
+    level = input("Lets play number guessing game, type 'easy'👍 or 'hard'👾: ")
+    attempts = 0
+    emojis = []
+    level,attempts,emojis = set_difficulty(level,attempts,emojis)
+    for i in range(attempts,0,-1):
+        print(f"you have {i},{emojis[i-1]} attempts left!")
+        guess = int(input("guess the number :"))
+        if compare(guess, number):
+            break
+        elif i==1:
+            print("You lose🧟‍♂️")
+
+# GAME STARTS HERE
+game()
 
 
